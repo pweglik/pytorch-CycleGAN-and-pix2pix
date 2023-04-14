dc1e7c42a572460df956bdd5bd1af95ea8995dae
pweglik <przemek.weglik@onet.pl>
2023-04-14 10:59:49 +0200

    Fix warning with learning rate

--- train.py
+++ train.py
@@ -40,7 +40,8 @@ if __name__ == '__main__':
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
         visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
-        model.update_learning_rate()    # update learning rates in the beginning of every epoch.
+        if epoch != opt.epoch_count:
+          model.update_learning_rate()    # update learning rates in the beginning of every epoch.
         for i, data in enumerate(dataset):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
             if total_iters % opt.print_freq == 0:
